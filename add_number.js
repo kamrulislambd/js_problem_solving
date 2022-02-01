d300c6bb7b18a4b2ac5a877f69203680982fd054
Kamrul Islam <upwork.kamrul@gmail.com>
2022-02-01 08:14:20 +0600

    math.js practice

---
+++ add_number.js
@@ -0,0 +1,6 @@
+function addNumbers(num1, num2){
+    let sum = num1 + num2;
+    return sum;
+}
+const addResult = addNumbers(56, 44);
+console.log(addResult);
---
+++ math.js
@@ -0,0 +1,15 @@
+let number = -7;
+let output = Math.abs(number);
+console.log(output);
+
+//Decimal to integer using Math.ceil()
+
+let myNumber = 21.5563;
+let result = Math.ceil(myNumber);
+
+console.log(result);
+
+//Decimal to Integer using Math.floor()
+
+let myResult = Math.floor(myNumber);
+console.log(myResult);
