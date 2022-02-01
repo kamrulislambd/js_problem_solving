cc97b5c740f323e7998e0150de9b94d9f811e1a1
Kamrul Islam <upwork.kamrul@gmail.com>
2022-02-01 10:40:45 +0600

    largest array element findout

--- arraysum.js
+++ arraysum.js
@@ -1,3 +1,5 @@
+/*
+
 function arraySum(numbers){
     let sum = 0;
 
@@ -10,3 +12,22 @@ function arraySum(numbers){
 
 const arraySumResult = arraySum([21, 18, 32, 58]);
 console.log(arraySumResult);
+
+*/
+
+function largestArray(numbers){
+    let largest = numbers[0];
+
+    for(let i = 0; i < numbers.length; i++){
+        let element = numbers[i];
+        if(element > largest){
+            largest = element;
+        }
+    }
+    return largest;
+}
+
+let listArray = [12, 24, 54, 22];
+const myLargest = largestArray(listArray);
+
+console.log(myLargest);
