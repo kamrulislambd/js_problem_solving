b55e76c12855fe1b9779dc13d16f30bb943a7377
Kamrul Islam <upwork.kamrul@gmail.com>
2022-02-01 11:35:21 +0600

    fibonacci.js updated

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,20 @@
-const fibo = [0, 1];
+function fibonacciSeries(num){
+    const fibo = [0, 1];
 
-for(let i = 2; i < 20; i++){
-    fibo[i] = fibo[i -1 ] + fibo[i - 2];
+    if(typeof num != 'number'){
+        return 'Please give a number';
+    }
+    if(num < 2){
+        return 'Please give a positive number and greater than 1';
+    }
+    
+        for(let i = 2; i <= num; i++){
+                fibo[i] = fibo[i -1 ] + fibo[i - 2];
+            }
+
+    return fibo;
+
 }
 
-console.log(fibo);
+const fiboSeries = fibonacciSeries(5);
+console.log(fiboSeries);
