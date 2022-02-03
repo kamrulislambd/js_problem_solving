43790f357e28741616c9fc00ed4255e76ec23feb
Kamrul Islam <upwork.kamrul@gmail.com>
2022-02-03 19:23:14 +0600

    area of circle

---
+++ circle-area.js
@@ -0,0 +1,25 @@
+
+
+//Area of Circle
+//Formula : area = PI * r * r;
+/*
+function areaOfCircle(radius){
+    return Math.PI * radius * radius;
+}
+console.log(areaOfCircle(2));
+
+*/
+
+//Function Expression
+/*
+const areaOfCircle = function(radius){
+    return Math.PI * radius * radius;
+}
+
+console.log(areaOfCircle(2));
+*/
+
+//Arrow Function
+
+const areaOfCircle = (radius) => Math.PI * radius * radius;
+console.log(areaOfCircle(2));
